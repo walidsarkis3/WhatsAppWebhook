26a21a5f51120bb0ad51ddc3e8db8636d2f82910
walidsarkis3 <walidsarkis3@gmail.com>
2025-11-29 00:27:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
 });
 
 // Route for POST requests
-app.post('/', (req, res) => {
+app.post('/', async(req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
